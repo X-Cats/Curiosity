fa93b35fcda43183e62cd083c3afb4668f8c09c0
Trevino <gtrevino@harris.com>
2018-02-11 16:19:27 -0500

    Added in rough Okhttp code, JSON parsing, and UI update code to retrieve list of events (currently only for 191) from TBA and updates spinner accordingly

--- Curiosity/app/src/main/java/xcats/app/ui/curiosity/MainActivity.java
+++ Curiosity/app/src/main/java/xcats/app/ui/curiosity/MainActivity.java
@@ -1,24 +1,128 @@
 package xcats.app.ui.curiosity;
 
 import android.content.Intent;
+import android.icu.text.LocaleDisplayNames;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    final String tbaKey = "U0rx6iHZYLFx1InrycvsfhYuxgRQPORyDM07f4Ekz2fHfftxJWAbIpzMD9SIl1sd";
+    final String tbaHeader = "X-TBA-Auth-Key";
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    List<String> eventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blueAllianceEventRetrieval();
+    }
+
+    private void blueAllianceEventRetrieval() {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+            .header(tbaHeader, tbaKey)
+            .url("http://www.thebluealliance.com/api/v3/team/frc191/events/2018/simple")
+            .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Unexpected code " + response);
+                }
+                else{
+                    eventList = parseResponse(response.body().byteStream());
+                    Log.d("parsedEventList",eventList.toString());
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateEventSpinner();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    private void updateEventSpinner() {
+        Spinner eventSpinner = findViewById(R.id.spinnerEvent);
+        ArrayAdapter<String> eventAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, eventList);
+        eventAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        eventSpinner.setAdapter(eventAdapter);
+    }
+
+    private List<String> parseResponse(InputStream response) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(response,"UTF-8"));
+        try {
+            return readMessagesArray(reader);
+        } finally {
+            reader.close();
+        }
+    }
+
+    public List<String> readMessagesArray(JsonReader reader) throws IOException {
+        List<String> messages = new ArrayList<>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            messages.add(readMessage(reader));
+        }
+        reader.endArray();
+        return messages;
+    }
+
+    public String readMessage(JsonReader reader) throws IOException {
+
+        String eventName = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("name")) {
+                eventName = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return eventName;
     }
 
     public void loginClick(View view){
